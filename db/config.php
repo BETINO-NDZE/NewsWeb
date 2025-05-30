4e44401cd122b4d671fe5a0dd5abef4b588c421d
BETINO-NDZE <150228775+BETINO-NDZE@users.noreply.github.com>
2025-05-30 12:43:40 +0100

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,8 +1,14 @@
 <?php
-	//connection database
-    
-    $con = mysqli_connect("localhost","root","")or die("unable to connect");
+$host = getenv('pg-c7780a6-bertrandndze15-9db2.j.aivencloud.com');
+$port = getenv('27530');
+$dbname = getenv('defaultdb');
+$user = getenv('avnadmin');
+$password = getenv('AVNS_a1viQEXxftPEXdLG04Q');
 
-    mysqli_select_db($con,"news"); 
+$connStr = "host=$host port=$port dbname=$dbname user=$user password=$password sslmode=require";
+$conn = pg_connect($connStr);
 
-?>
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+echo "Connected to Aiven PostgreSQL successfully!";
